ed30369f9304f703515a0f47ecd1a5141cddb373
Igam22 <103739751+Igam22@users.noreply.github.com>
2023-06-20 18:16:33 +0200

    Update ProductList.kt

--- app/src/main/java/shop/ebusiness/model/ProductList.kt
+++ app/src/main/java/shop/ebusiness/model/ProductList.kt
@@ -14,6 +14,7 @@ class ProductList {
     var fet:Boolean=false
     var kal:Boolean=false
     var pr:Double=0.0
+    lateinit var einh_preis:String
 }
 
 val productList: List<ProductList> = listOf(
