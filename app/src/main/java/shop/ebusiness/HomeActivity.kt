e8de6fa92e27926f06c37913090502f617c85b18
Igam22 <103739751+Igam22@users.noreply.github.com>
2023-06-20 18:06:26 +0200

    Create HomeActivity.kt

---
+++ app/src/main/java/shop/ebusiness/HomeActivity.kt
@@ -0,0 +1,24 @@
+class HomeActivity : ComponentActivity() {
+    private lateinit var dbManager: DbManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+        dbManager = DbManager(this)
+        dbManager.open()
+
+        val searchBar: SearchView = findViewById(R.id.search_bar1)
+        searchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(searchInput: String?): Boolean {
+                val intent = Intent(this@HomeActivity, SearchActivity::class.java)
+                intent.putExtra("searchQuery", searchInput)
+                startActivity(intent)
+                return true
+            }
+        })
+    }
+}
