dc07e8f0322fe0821c263155798fe9a494d8705a
Lisella <38525372+Lisella@users.noreply.github.com>
2023-06-21 11:53:33 +0200

    modified ProductView

--- app/src/main/java/shop/ebusiness/util/Buttons.kt
+++ app/src/main/java/shop/ebusiness/util/Buttons.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -44,7 +45,7 @@ fun DecreaseIcon() {
         colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF4FC0B3))
     ) {
         Icon(
-            imageVector = Icons.Default.Delete,
+            imageVector = Icons.Default.Remove,
             contentDescription = "Minus",
             tint = MaterialTheme.colorScheme.onSurface
         )
