c505369619a48ebf95a5c97f0d42f40ce22fbd2c
Barryrn <87810004+Barryrn@users.noreply.github.com>
2023-06-21 17:25:33 +0200

    Add files via upload

---
+++ src/main/java/com/example/blutwert/DataBaseHelper.java
@@ -0,0 +1,107 @@
+package com.example.blutwert;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+
+    public static final String CUSTOMER_TABLE = "CUSTOMER_TABLE";
+    public static final String COLUMN_CUSTOMER_DATUM = "CUSTOMER_DATUM";
+    public static final String COLUMN_CUSTOMER_EISENGEHALT = "CUSTOMER_EISENGEHALT";
+    public static final String COLUMN_CUSTOMER_CHOLOSTERINGEHALT = "CUSTOMER_CHOLOSTERINGEHALT";
+    public static final String COLUMN_CUSTOMER_BLUTZUCKER = "CUSTOMER_BLUTZUCKER";
+    public static final String COLUMN_ID = "ID";
+    public static final String COLUMN_CUSTOMER_TRIGLYCERIDE = "CUSTOMER_TRIGLYCER" + COLUMN_ID + "E";
+    public static final String COLUMN_CUSTOMER_BLUTDRUCK = "CUSTOMER_BLUTDRUCK";
+
+    public static final String COLUMN_CUSTOMER_VITAMIND = "CUSTOMER_VITAMIND";
+    public static final String COLUMN_CUSTOMER_VITAMINB_12 = "CUSTOMER_VITAMINB12";
+
+    public DataBaseHelper(@Nullable Context context) {
+        super(context, "customer.db", null, 1);
+    }
+
+    //this is called thr first time a database is created. There should be code in here to create a new database.
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String crateTableStatement = "CREATE TABLE " + CUSTOMER_TABLE + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_CUSTOMER_DATUM + " TEXT, " + COLUMN_CUSTOMER_EISENGEHALT + " INT, " + COLUMN_CUSTOMER_CHOLOSTERINGEHALT + " INT, " + COLUMN_CUSTOMER_BLUTZUCKER + " INT, " + COLUMN_CUSTOMER_TRIGLYCERIDE + " INT, " + COLUMN_CUSTOMER_BLUTDRUCK + " INT, " + COLUMN_CUSTOMER_VITAMIND + " INT," + COLUMN_CUSTOMER_VITAMINB_12 + " INT)";
+
+        db.execSQL(crateTableStatement);
+    }
+
+
+    //This is called if the database version number changes. It prevents previous users app from breaking when you change database design.
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public boolean addOne(CustomerModel customerModel){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_CUSTOMER_DATUM, customerModel.getDatum());
+        cv.put(COLUMN_CUSTOMER_EISENGEHALT, customerModel.getEisengehalt());
+        cv.put(COLUMN_CUSTOMER_CHOLOSTERINGEHALT, customerModel.getCholesteringehalt());
+        cv.put(COLUMN_CUSTOMER_BLUTZUCKER, customerModel.getBlutzucker());
+        cv.put(COLUMN_CUSTOMER_TRIGLYCERIDE, customerModel.getTriglyceride());
+        cv.put(COLUMN_CUSTOMER_BLUTDRUCK, customerModel.getBlutdruck());
+        cv.put(COLUMN_CUSTOMER_VITAMIND, customerModel.getVitaminD());
+        cv.put(COLUMN_CUSTOMER_VITAMINB_12, customerModel.getVitaminB12());
+
+        long insert = db.insert(CUSTOMER_TABLE,null ,cv);
+        if(insert == -1){
+            return  false;
+        }
+        else{
+            return true;
+        }
+    }
+}
+/*
+    public List<CustomerModel>getEveryone() {
+        List<CustomerModel> returnList = new ArrayList<>();
+        //get data from the database
+
+        String queryString = "SELECT * FROM " + CUSTOMER_TABLE;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(queryString, null);
+        if (cursor.moveToFirst()) {
+            // loop trough the cursor (result set) and create new customer objects. Put them into return list.
+            do {
+                int CustomerID = cursor.getInt(0);
+                String Datum = cursor.getString(1);
+                int Eisengehalt = cursor.getInt(2);
+                int Cholesteringehalt = cursor.getInt(3);
+                int Blutzucker = cursor.getInt(4);
+                int Triglyceride = cursor.getInt(5);
+                int Blutdruck = cursor.getInt(6);
+                int VitaminD = cursor.getInt(7);
+                int VitaminB12 = cursor.getInt(8);
+
+                CustomerModel newCustomer = new CustomerModel(CustomerID, Datum, Eisengehalt, Cholesteringehalt, Blutzucker, Triglyceride, Blutdruck, VitaminD, VitaminB12);
+                returnList.add(newCustomer);
+
+            } while (cursor.moveToNext());
+        } else {
+            //failure. do not add anything to the List
+        }
+        // close both the cursor and the db when done
+        cursor.close();
+        db.close();
+        return returnList;
+    }
+}
+*/
---
+++ src/main/java/com/example/blutwert/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.blutwert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    //references to buttons amd otter controls on the layouts
+    Button btn_add, btn_viewAll;
+    EditText et_datum, et_eisengehalt, et_cholesteringehalt, et_blutzucker, et_triglyceride, et_blutdruck, et_vitamind, et_vitaminb12;
+    ListView lv_customerList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_add = findViewById(R.id.btn_add);
+        btn_viewAll = findViewById(R.id.btn_viewAll);
+        btn_viewAll.setVisibility(View.GONE);
+        et_datum = findViewById(R.id.et_datum);
+        et_eisengehalt = findViewById(R.id.et_eisengehalt);
+        et_cholesteringehalt = findViewById(R.id.et_cholesteringehalt);
+        et_blutzucker = findViewById(R.id.et_blutzucker);
+        et_triglyceride = findViewById(R.id.et_triglyceride);
+        et_blutdruck = findViewById(R.id.et_blutdruck);
+        et_vitamind = findViewById(R.id.et_vitamind);
+        et_vitaminb12 = findViewById(R.id.et_vitaminb12);
+        lv_customerList = findViewById(R.id.lv_customerList);
+
+
+        //button listerners for the add and view all buttons
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                CustomerModel customerModel;
+
+                try{
+                     customerModel = new CustomerModel(
+                            -1,
+                            et_datum.getText().toString(),
+                            Integer.parseInt(et_eisengehalt.getText().toString()),
+                            Integer.parseInt(et_cholesteringehalt.getText().toString()),
+                            Integer.parseInt(et_blutzucker.getText().toString()),
+                            Integer.parseInt(et_triglyceride.getText().toString()),
+                            Integer.parseInt(et_blutdruck.getText().toString()),
+                            Integer.parseInt(et_vitamind.getText().toString()),
+                            Integer.parseInt(et_vitaminb12.getText().toString())
+                    );
+
+                    Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e){
+                    Toast.makeText(MainActivity.this, "Error Creating Customer", Toast.LENGTH_SHORT).show();
+                    customerModel = new CustomerModel(-1, "error", 0, 0,0,0,0,0,0);
+                }
+
+                DataBaseHelper dataBaseHelper= new DataBaseHelper(MainActivity.this);
+                boolean success = dataBaseHelper.addOne(customerModel);
+                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+        btn_viewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
+                //List<CustomerModel> everyone = dataBaseHelper.getEveryone();
+                Toast.makeText(MainActivity.this, "everyone.toString()", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+    }
+}
